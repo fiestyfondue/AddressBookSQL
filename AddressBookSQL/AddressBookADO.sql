b124fe3161a7956789c6308968ec18ed78d94579
Harsh <harsh.21483@gmail.com>
2021-07-13 12:36:27 -0700

    [Harsh]Refactor:-Insering data and Editing it

--- AddressBookSQL/AddressBookADO.sql
+++ AddressBookSQL/AddressBookADO.sql
@@ -15,3 +15,12 @@ email varchar(255)
 
 --displaying table
 select * from ContactBook
+
+--Inserting Data--
+INSERT INTO ContactBook(firstname,lastname,address,city,state,zip,phoneNumber,email)
+VALUES ('Harsh','Sinha','MR4 Road','Delhi','Punjab',384794,9847558470,'whatisemail@gmail.com'),
+('Aman','Patel','North Street','Vancover','Canada',434209,912343467,'Vancoveritis6@gmail.com'),
+('Rahul','Hdas','St. Mathew Road','Regina','Canada',834510,882234322,'rahulisthename@gmail.com'),
+('Paesd','Srerar','Asgard','ThorRoad','Space',438324,73454765,'SpaceIsReal@gmail.com');
+UPDATE ContactBook SET phoneNumber = 8827880778
+WHERE firstname = 'Aman' AND lastname= 'Patel'
